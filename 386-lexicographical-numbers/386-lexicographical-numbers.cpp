9f95a909f296493c396ee507947316e366306882
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-10-05 22:16:31 +0530

    Time: 137 ms (20.94%), Space: 34.6 MB (5.03%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -1,20 +1,49 @@
-bool cmp(string &a,string &b){
-    return a<b;
-}
+struct node{
+    node*links[10];
+    bool flag=false;
+    string str="";
+};
+class Trie {
+public:
+    node*root;
+    Trie() {
+        root = new node();
+    }
+    
+    void insert(string s) {
+        node*temp = root;
+        for(int i=0;i<s.size();i++){
+            if(temp->links[s[i]-'0']==NULL){
+                temp->links[s[i]-'0'] =  new node();
+            }
+            temp = temp->links[s[i]-'0'];
+        }
+        temp->flag = true;
+        temp->str = s;
+    }
+    
+    void search(node*temp,vector<int>&sol) {
+        if(temp->flag){
+            sol.push_back(stoi(temp->str));
+        }
+       for(auto i:temp->links)
+           if(i!=NULL){
+               search(i,sol);
+           }
+    }
+    };
 class Solution {
 public:
     
     vector<int> lexicalOrder(int n) {
         vector<string>ans;
+        Trie*t = new Trie();
         for(int i=1;i<=n;i++){
             string mid = to_string(i);
-            ans.push_back(mid);
+            t->insert(mid);
         }
         vector<int>sol;
-        sort(ans.begin(),ans.end(),cmp);
-        for(auto i:ans){
-            sol.push_back(stoi(i));
-        }
+        t->search(t->root,sol);
         return sol;
     }
 };
