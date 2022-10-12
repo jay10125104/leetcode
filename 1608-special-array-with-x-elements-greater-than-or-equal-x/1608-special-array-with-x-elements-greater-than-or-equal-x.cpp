7905bb01940abd7ae586611210c13179e41cdd84
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-10-12 21:54:29 +0530

    Time: 7 ms (39.29%), Space: 8.3 MB (55.34%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool sol(vector<int>&nums,int mid,int &flag){
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>=mid){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        flag=count;
+        return count>=mid;
+    
+    }
+    int specialArray(vector<int>& nums) {
+        int ans=-1;
+        int low = 1;
+        sort(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.end());
+        int high = nums.size();
+        while(low<=high){
+            int mid = (low+high)/2;
+            int flag=0;
+            if(sol(nums,mid,flag)){
+                if(flag==mid)
+                ans = mid;
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+};
