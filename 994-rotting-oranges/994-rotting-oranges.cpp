ad2a29fec18a7ab1adea122ac94ea3917e0bfaa6
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-10-15 00:59:50 +0530

    Time: 7 ms (89.63%), Space: 13.2 MB (35.49%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,54 +1,58 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& v) {
-        int count = 0;
         queue<pair<int,int>>q;
-        vector<vector<int>>visited(v.size(),vector<int>(v[0].size()));
+        int count=0;
+        int m=v.size();
+        int n=v[0].size();
+       vector<vector<int>>visited(m,vector<int>(n,0));
         for(int i=0;i<v.size();i++){
             for(int j=0;j<v[0].size();j++){
-                if(v[i][j]==2){
+                if(v[i][j]==1){
+                    count++;
+                }
+                else if(v[i][j]==2){
                     q.push({i,j});
                     visited[i][j]=1;
-                }
-                else if(v[i][j]){
-                    count++;
                 }
             }
         }
         int ans=0;
         while(q.size() && count){
-            int x = q.size();
             ans++;
+            int x = q.size();
             while(x--){
                 auto it = q.front();
-                int  i = it.first;
-                int j=it.second;
+                int i = it.first;
                 q.pop();
-                if(i-1>=0 && v[i-1][j] && visited[i-1][j]==0){
+                int j = it.second;
+                if(i-1>=0 && visited[i-1][j]==0 && v[i-1][j]==1){
                     q.push({i-1,j});
                     visited[i-1][j]=1;
                     count--;
                 }
-                if(i+1<v.size() && v[i+1][j] && visited[i+1][j]==0){
-                    q.push({i+1,j});
-                    visited[i+1][j]=1;
-                    count--;
-                }
-                if(j-1>=0 && v[i][j-1] && visited[i][j-1]==0){
+                if(j-1>=0 && visited[i][j-1]==0 && v[i][j-1]==1){
                     q.push({i,j-1});
                     visited[i][j-1]=1;
                     count--;
                 }
-                if(j+1<v[0].size() && v[i][j+1] && visited[i][j+1]==0){
+                if(i+1<m && visited[i+1][j]==0 && v[i+1][j]==1){
+                    q.push({i+1,j});
+                    visited[i+1][j]=1;
+                    count--;
+                }
+                if(j+1<n && visited[i][j+1]==0 && v[i][j+1]==1){
                     q.push({i,j+1});
                     visited[i][j+1]=1;
                     count--;
                 }
             }
         }
-        if(!count){
+        if(count==0){
             return ans;
         }
-        return -1;
+        else{
+            return -1;
+        }
     }
 };
