a14b95dbf3fce3d66cd98b6db16b5f2bdefaa6e1
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-10-15 00:39:44 +0530

    Time: 238 ms (90.82%), Space: 72.6 MB (21.23%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& p, vector<vector<int>>&q) {
+        // map<pair<int,int>>m;
+        vector<int>adj[n];
+        for(auto i:p){
+            adj[i[0]].push_back(i[1]);
+        }
+        vector<vector<int>>visited(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            queue<int>q;
+            q.push(i);
+            vector<int>vis(n);
+            vis[i]=1;
+            while(q.size()){
+                int x = q.front();
+                q.pop();
+                for(auto it:adj[x]){
+                    if(visited[i][it]==0){
+                        visited[i][it]=1;
+                        q.push(it);
+                    }
+                }
+            }
+        }
+        vector<bool>sol;
+        for(auto i:q){
+            if(visited[i[0]][i[1]]){
+                sol.push_back(true);
+            }
+            else{
+                sol.push_back(false);                
+            }
+        }
+        return sol;
+        
+    }
+};
