7ec630a30316bfb443eee970dc3f3fea12047145
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-10-18 23:20:29 +0530

    Time: 1809 ms (5.01%), Space: 209 MB (8.49%) - LeetHub

---
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int count=0;
+    void dfs(vector<int>adj[],int src,vector<int>&visited,map<int,int>&m){
+        if(m[src]){
+            return;
+        }
+        count++;
+        visited[src]=1;
+        for(auto it:adj[src]){
+            if(visited[it]==0)
+            dfs(adj,it,visited,m);
+        }
+    }
+    int reachableNodes(int n, vector<vector<int>>& v, vector<int>& r) {
+        vector<int>adj[n];
+        for(auto i:v){
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+        vector<int>visited(n);
+        map<int,int>m;
+        for(auto i:r){
+            m[i]++;
+        }
+        dfs(adj,0,visited,m);
+            return count;
+    }
+};
