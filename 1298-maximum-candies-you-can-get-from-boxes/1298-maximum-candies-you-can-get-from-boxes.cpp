94477a4051ed1dcbd3b8a4f50469bd6aabb2bbbd
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-10-20 12:39:01 +0530

    Time: 476 ms (5.45%), Space: 41.7 MB (12.87%) - LeetHub

---
+++ 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& init) {
+        set<int>s;
+        int n=status.size();
+        for(int i=0;i<status.size();i++){
+            if(status[i]){
+                s.insert(i);
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<init.size();i++){
+            q.push(init[i]);
+        }
+        vector<int>visited(n);
+        int ans=0;
+        map<int,int>prev;
+        while(q.size()){
+            int x = q.front();
+            q.pop();
+            // cout<<x<<" ";
+            if(s.count(x)){
+                ans+=candies[x];
+                candies[x]=0;
+            }
+            else{
+                prev[x]=1;
+                continue;
+            }
+            for(auto i:keys[x]){
+                s.insert(i);
+                if(prev[i]!=0){
+                    q.push(i);
+                    prev[i]=0;
+                }
+            }
+            for(auto i:containedBoxes[x]){
+                q.push(i);
+            }
+        }
+        return ans;
+    }
+};
