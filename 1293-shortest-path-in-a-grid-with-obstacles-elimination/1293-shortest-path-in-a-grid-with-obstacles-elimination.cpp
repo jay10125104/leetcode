0a9210edfb9af92f533fe92e671a7e04219be978
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-10-22 00:59:47 +0530

    Time: 643 ms (18.83%), Space: 58.2 MB (21.24%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int shortestPath(vector<vector<int>>& v, int k) {
+        int m=v.size();
+        int n=v[0].size();
+        int dp[m][n][k+1];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         for(int t=0;t<=k;t++){
+        //             dp[i][j][t]=0;
+        //         }
+        //     }
+        // }
+        memset(dp,0,sizeof(dp));
+        queue<vector<int>>q;
+        q.push({0,0,0,k});
+        vector<int>x = {0,1,0,-1};
+        vector<int>y = {-1,0,1,0};
+        dp[0][0][k]=1;
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            dp[it[0]][it[1]][it[3]]=1;
+            if(it[0]==m-1 && it[1]==n-1){
+                return it[2];
+            }
+            for(int i=0;i<4;i++){
+                int newx = it[0]+x[i];
+                int newy = it[1]+y[i];
+                if(newx>=0 && newy>=0 && newx<m && newy<n ){
+                    if(v[newx][newy] && it[3] &&  dp[newx][newy][it[3]-1]==0){
+                        q.push({newx,newy,it[2]+1,it[3]-1});
+                        dp[newx][newy][it[3]-1]=1;
+                        if(newx==m-1 && newy==n-1){
+                            return it[2]+1;
+                        }
+                    }
+                    else if(v[newx][newy]==0 && dp[newx][newy][it[3]]==0){
+                        q.push({newx,newy,it[2]+1,it[3]});
+                        dp[newx][newy][it[3]]=1;
+                        if(newx==m-1 && newy==n-1){
+                            return it[2]+1;
+                        }
+                    }
+                }
+            }
+            
+        }
+        return -1;
+    }
+};
