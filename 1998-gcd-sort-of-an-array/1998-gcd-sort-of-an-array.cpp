8cf56b263467eace8917e45a403d4b527ee59f35
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-10-22 13:04:28 +0530

    Time: 687 ms (71.55%), Space: 98.9 MB (48.54%) - LeetHub

---
+++ 1998-gcd-sort-of-an-array/1998-gcd-sort-of-an-array.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    struct unionfind{
+      vector<int>parent;
+        unionfind(int n){
+            parent.resize(n);
+            for(int i=0;i<n;i++){
+                parent[i]=i;
+            }
+            
+        }
+        int find(int node){
+            if(parent[node]==node){
+                return node;
+            }
+            else{
+                return parent[node] = find(parent[node]);
+            }
+        }
+        bool Union(int u,int v){
+            u = find(u);
+            v = find(v);
+            if(u!=v){
+                parent[v] = u;
+                return false;
+            }
+            return true;
+        }
+    };
+    bool gcdSort(vector<int>& v) {
+        
+        unionfind u = unionfind(100001);
+        for(auto &j:v){
+            int n=j;
+            if(n%2==0){
+                u.Union(j,2);
+            }
+            while (n % 2 == 0)
+    {
+        n = n/2;
+    }
+    for (int i = 3; i <= sqrt(n); i = i + 2)
+    {
+        // While i divides n, print i and divide n
+        if(n%i==0){
+            u.Union(j,i);
+        }
+        while (n % i == 0)
+        {
+           
+            n = n/i;
+        }
+    }
+ 
+    // This condition is to handle the case when n
+    // is a prime number greater than 2
+    if (n > 2){
+        u.Union(j,n);
+    }
+        }
+    
+        vector<pair<int,int>>p;
+        for(int i=0;i<v.size();i++){
+            p.push_back({v[i],i});
+        }
+        sort(p.begin(),p.end());
+        for(int i=0;i<v.size();i++){
+            int first = p[i].first;
+            int second = v[i];
+            if(u.find(first)!=u.find(second)){
+                // cout<<first<<" "<<second<<endl;
+                return false;
+            }
+        }
+        return true;
+    }
+};
