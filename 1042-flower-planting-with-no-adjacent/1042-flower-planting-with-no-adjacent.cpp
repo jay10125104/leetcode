d803676aee82276417d89cd2f7303586ce47f134
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-10-30 17:40:10 +0530

    Time: 142 ms (93.62%), Space: 43.5 MB (67.41%) - LeetHub

---
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> gardenNoAdj(int n, vector<vector<int>>& graph) {
+        vector<int>adj[n];
+        for(auto &i:graph){
+            adj[i[0]-1].push_back(i[1]-1);
+            adj[i[1]-1].push_back(i[0]-1);
+        }
+        vector<int>ans(n,0);
+        vector<int>visited(n);
+        // cout<<"sdf";
+        for(int i=0;i<n;i++){
+            vector<int>v(5);
+            for(auto it:adj[i]){
+                v[ans[it]]++;
+            }
+            int j;
+            for(j=1;j<=4;j++){
+                if(v[j]==0){
+                    break;
+                }
+            }
+            // cout<<1;   
+            ans[i] = j;
+        }
+    
+    return ans;
+    }
+};
