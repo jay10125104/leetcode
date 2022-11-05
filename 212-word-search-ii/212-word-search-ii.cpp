b360244abaecc90d036229c46003c0f8d134ce07
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-11-05 10:53:40 +0530

    Time: 1617 ms (36.97%), Space: 16 MB (18.48%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -54,9 +54,10 @@ public:
         int n=v[0].size();
         vector<string>sol;
         set<string>sol2;
+                vector<vector<int>>visited(m,vector<int>(n,0));
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                vector<vector<int>>visited(m,vector<int>(n,0));
                 node*temp = t->root;
                 ans(v,temp,i,j,visited,sol2);
             }
