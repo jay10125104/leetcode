fc8c9d6edec5d227934e364922cc82f7089586bc
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-11-05 11:06:48 +0530

    Time: 794 ms (75.20%), Space: 15.4 MB (30.86%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -1,70 +1,64 @@
 struct node{
-    node*links[26];
-    bool flag=false;
-    string real="";
+  node*links[26];
+    string str="";
+    int flag=0;
 };
-class Trie {
-public:
+class Trie{
+    public:
     node*root;
-    Trie() {
-        root = new node();
-    }
-    
-    void insert(string s) {
+  Trie(){
+      root = new node();
+  }
+    void insert(string s){
         node*temp = root;
         for(int i=0;i<s.size();i++){
             if(temp->links[s[i]-'a']==NULL){
-                temp->links[s[i]-'a'] =  new node();
+                temp->links[s[i]-'a'] = new node();
             }
-            temp = temp->links[s[i]-'a'];
+           temp=temp->links[s[i]-'a'];
         }
-        temp->real = s;
-        temp->flag = true;
+        temp->flag = 1;
+        temp->str = s;
     }
 };
+void dfs(vector<vector<char>>&v,int i,int j,node*temp,vector<vector<int>>&visited,vector<string>&ans){
+    if(i<0 || j<0 || i==v.size() || j == v[0].size() || visited[i][j]==1){
+        return;
+    }
+    if(temp->links[v[i][j]-'a']==NULL){
+        return;
+    }
+    else{
+        temp = temp->links[v[i][j]-'a'];
+        if(temp->flag){
+            ans.push_back(temp->str);
+            temp->flag=0;
+        }
+    }
+    visited[i][j]=1;
+    dfs(v,i,j-1,temp,visited,ans);
+    dfs(v,i,j+1,temp,visited,ans);
+    dfs(v,i-1,j,temp,visited,ans);
+    dfs(v,i+1,j,temp,visited,ans);
+    visited[i][j]=0;
+}
 class Solution {
 public:
-    void ans(vector<vector<char>>&v,node*temp,int i,int j,vector<vector<int>>&visited,set<string>&sol){
-         if(i<0 || j<0 || i>=v.size()||j>=v[0].size()){
-            return;
-        }
-        if(temp->links[v[i][j]-'a']==NULL || visited[i][j]==1){
-            return;
-        }
-        else{
-            temp=temp->links[v[i][j]-'a'];
-            if(temp->flag){
-            sol.insert(temp->real);
-            }
-        }
-        
-        visited[i][j]=1;
-        ans(v,temp,i,j-1,visited,sol);
-        ans(v,temp,i+1,j,visited,sol);
-        ans(v,temp,i-1,j,visited,sol);
-        ans(v,temp,i,j+1,visited,sol);
-        visited[i][j]=0;
-    }
-    vector<string> findWords(vector<vector<char>>& v, vector<string>& s) {
-        Trie *t = new Trie();
-        for(auto i:s){
+    vector<string> findWords(vector<vector<char>>& v, vector<string>& words) {
+        vector<string>ans;
+        Trie *t = new Trie(); 
+        for(auto &i:words){
             t->insert(i);
         }
         int m=v.size();
-        int n=v[0].size();
-        vector<string>sol;
-        set<string>sol2;
-                vector<vector<int>>visited(m,vector<int>(n,0));
-        
+        int n = v[0].size();
+        vector<vector<int>>visited(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 node*temp = t->root;
-                ans(v,temp,i,j,visited,sol2);
+                dfs(v,i,j,temp,visited,ans);
             }
         }
-        for(auto &i:sol2){
-            sol.push_back(i);
-        }
-        return sol;
+        return ans;
     }
 };
