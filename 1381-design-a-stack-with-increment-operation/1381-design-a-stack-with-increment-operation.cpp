0413d970ec1f2007e20cb3b43c63f0a695c01f36
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-11-05 10:18:53 +0530

    Time: 225 ms (5.04%), Space: 37.2 MB (5.76%) - LeetHub

---
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
@@ -0,0 +1,43 @@
+class CustomStack {
+public:
+    int mx;
+    stack<int>s;
+    CustomStack(int maxSize) {
+        mx = maxSize;
+    }
+    
+    void push(int x) {
+        if(s.size()<mx){
+            s.push(x);
+        }
+    }
+    
+    int pop() {
+        if(s.size()){
+            int x = s.top();
+            s.pop();
+            return x;
+        }
+        else{
+            return -1;
+        }
+    }
+    
+    void increment(int k, int val) {
+        stack<int>st;
+        k = min(k,(int)s.size());
+        while(s.size()){
+            if(s.size()<=k){
+                st.push(s.top()+val);
+            }
+            else{
+                st.push(s.top());
+            }
+            s.pop();
+        }
+        while(st.size()){
+            s.push(st.top());
+            st.pop();
+        }
+    }
+};
