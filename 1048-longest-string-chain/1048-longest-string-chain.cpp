c280ae03486ce1b46db21e4f09c0c086123ebed3
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-11-22 22:00:12 +0530

    Time: 2350 ms (5.03%), Space: 321.4 MB (5.45%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,47 @@
+bool cmp(string &s,string &t){
+    return s.size()<t.size();
+}
+class Solution {
+public:
+    int longestStrChain(vector<string>& v) {
+        sort(v.begin(),v.end(),cmp);
+        int n=v.size();
+        int dp[n];
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+        int ans = 1;
+        for(int i=1;i<n;i++){
+            // cout<<v[i]<<" ";
+            string s = v[i];
+            int sz = s.size();
+            dp[i]=1;
+            for(int j=i-1;j>=0;j--){
+                string t = v[j];
+                if((sz-t.size())>=2){
+                    break;
+                }
+                else if(sz-t.size()){
+                    int x=0;
+                    int y=0;
+                    int sol=0;
+                    while(x<sz && y<t.size()){
+                        if(s[x]!=t[y]){
+                            sol++;
+                            x++;
+                        }
+                        else{
+                            x++;
+                            y++;
+                        }
+                    }
+                    if(sol<=1){
+                        // cout<<v[i]<<" "<<v[j]<<endl;
+                        dp[i] = max(dp[i],dp[j]+1);
+                    }
+                }
+            }
+            ans = max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
