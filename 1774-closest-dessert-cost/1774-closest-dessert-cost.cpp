a1294b93d1ba18f05ae95792017887e747a9ac10
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-12-01 18:41:35 +0530

    Time: 800 ms (5.23%), Space: 9.9 MB (54.53%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.cpp
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.cpp
@@ -1,52 +1,30 @@
 class Solution {
 public:
-    int closestCost(vector<int>& v, vector<int>& t, int target) {
-        int n=v.size();
-        int ans=INT_MAX;
-        int diff = INT_MAX;
-        for(auto &it:v){
-            int x = it;
-            int a =pow(2,t.size());
-            for(int i=0;i<a;i++){
-                int mid1=0;
-                int tt = i;
-                int count=0;
-                while(tt){
-                    if(tt&1){
-                        mid1+=t[count];
-                    }
-                    count++;
-                    tt =tt/2;
-                }
-                for(int j=0;j<a;j++){
-                 int mid2=0;
-                    count=0;
-                    tt = j;
-                    while(tt){
-                    if(tt&1){
-                        mid2+=t[count];
-                    }
-                    count++;
-                    tt =tt/2;
-                }
-                    int sol = x + mid2 + mid1;
-                    int mid3 = abs(sol-target);
-                    if(diff>mid3){
-                        diff = mid3;
-                        ans = sol;
-                    }
-                    else if(diff==mid3){
-                        ans = min(ans,sol);
-                    }
-                    if(diff==0){
-                        return target;
-                    }
-                    
-                    
-                }
+    int ans,diff = INT_MAX;
+    void sol(vector<int>&t,int target,int idx,int c){
+            if(abs(c-target)==diff){
+                ans = min(ans,c);
             }
+            else if(abs(c-target)<diff){
+                diff=abs(c-target);
+                ans = c;
+            }
+        
+        if(idx==t.size()){
+            return;
+        }
+        sol(t,target,idx+1,c);
+        sol(t,target,idx+1,c+t[idx]);
+        
+    }
+    int closestCost(vector<int>& v, vector<int>& t, int target) {
+        int n=t.size();
+        for(int i=0;i<n;i++){
+            t.push_back(t[i]);
+        }
+        for(auto &i:v){
+            sol(t,target,0,i);
         }
         return ans;
-        
     }
 };
