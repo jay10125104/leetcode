abd5e0c1cf05061721d005375096c5881445e30a
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-12-09 00:30:24 +0530

    Time: 2189 ms (12.12%), Space: 14.2 MB (23.15%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int dp[17][1<<16];
+    bool ans(vector<int>&nums,int count,int k,int mask,int target){
+        if(count==k){
+            return true;
+        }
+        else if(dp[count][mask]!=-1){
+            return dp[count][mask];
+        }
+        else{
+            int t = pow(2,nums.size());
+            for(int i=0;i<t;i++){
+                if(mask&i){
+                    continue;
+                }
+                int sum=0;
+                for(int j=0;j<16;j++){
+                    int Mask = pow(2,j);
+                    if(Mask&i){
+                        sum+=nums[j];
+                    }
+                    if(sum>target){
+                        break;
+                    }
+                }
+                if(sum==target){
+                    if(ans(nums,count+1,k,mask|i,target)){
+                        return dp[count][mask]= true;
+                    }
+                }
+            }
+            return dp[count][mask] = false;
+        }
+    }
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum=0;
+        for(auto &i:nums){
+            sum+=i;
+        }
+        if(sum%k){
+            return 0;
+        }
+        else{
+            memset(dp,-1,sizeof(dp));
+            return ans(nums,0,k,0,sum/k);
+        }
+    }
+};
