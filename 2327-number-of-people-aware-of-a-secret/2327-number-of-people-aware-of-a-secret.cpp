cf040477dfdcfe56d4f900771db5499d30b969b5
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-12-24 12:20:23 +0530

    Time: 76 ms (30.49%), Space: 5.8 MB (98.37%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        int dp[n];
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+        int mod = 1000000007;
+        for(int i=0;i<n;i++){
+            for(int j=delay;j<forget && ((i+j)<n);j++){
+                dp[i+j] = (dp[i]%mod+dp[i+j]%mod)%mod;
+            }
+            cout<<dp[i]<<" "<<endl;
+        }
+        int i=n-1;
+        int ans=0;
+        while(i>=0 && forget){
+            forget--;
+            ans= (ans%mod + dp[i]%mod)%mod;
+            i--;
+        }
+        return ans;
+        
+    }
+};
