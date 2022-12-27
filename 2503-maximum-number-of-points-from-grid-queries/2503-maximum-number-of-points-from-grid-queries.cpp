0fca863211e1335fcb024f2ff802811cce88b0a3
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-12-27 11:13:45 +0530

    Time: 1844 ms (7.99%), Space: 397.5 MB (5.03%) - LeetHub

--- 2503-maximum-number-of-points-from-grid-queries/2503-maximum-number-of-points-from-grid-queries.cpp
+++ 2503-maximum-number-of-points-from-grid-queries/2503-maximum-number-of-points-from-grid-queries.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& qi) {
-        unordered_map<int,int>ans;
+        map<int,int>ans;
         int m=grid.size();
         int n=grid[0].size();
         queue<pair<int,int>>q;
         vector<int>mid = qi;
         sort(mid.begin(),mid.end());
         int count=0;
-        queue<pair<int,int>>q1;
-        int flag=0;
          int visited[m][n];
             memset(visited,0,sizeof(visited));
             q.push({0,0});
@@ -19,7 +17,7 @@ public:
             if(ans.find(t)!=ans.end()){
                 continue;
             }
-            if(flag==0){
+            queue<pair<int,int>>q1;
            
             while(q.size()){
                 auto it = q.front();
@@ -27,6 +25,7 @@ public:
                 int y = it.second;
                 q.pop();
                 if(grid[x][y]>=t){
+                // cout<<x<<" "<<y<<" "<<t<<endl;
                     q1.push({x,y});
                 }
                 else{
@@ -50,41 +49,7 @@ public:
                     count++;
                 }
             }
-                flag=1;
-            }
-            else{
-                while(q1.size()){
-                auto it = q1.front();
-                int x = it.first;
-                int y = it.second;
-                q1.pop();
-                if(grid[x][y]>=t){
-                    q.push({x,y});
-                }
-                else{
-                    if((x-1)>=0 && visited[x-1][y]==0){
-                        q1.push({x-1,y});
-                        visited[x-1][y]=1;
-                    }
-                    if((x+1)<m && visited[x+1][y]==0){
-                        q1.push({x+1,y});
-                        visited[x+1][y]=1;
-                        
-                    }
-                    if((y-1)>=0 && visited[x][y-1]==0){
-                        q1.push({x,y-1});
-                        visited[x][y-1]=1;
-                    }
-                    if((y+1)<n && visited[x][y+1]==0){
-                        q1.push({x,y+1});
-                        visited[x][y+1]=1;
-                    }
-                    count++;
-                }
-            }
-                flag=0;
-            }
-            
+            q=q1;
             // cout<<count<<endl;
             ans[t]=count;
         }
