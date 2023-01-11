1ac3a744254dcb1f34c7b311aef9d68da256321c
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-01-11 23:41:45 +0530

    Time: 28 ms (91.49%), Space: 10.6 MB (61.50%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -1,55 +1,44 @@
 class Solution {
 public:
     int takeCharacters(string s, int k) {
+        int i=0;
         int a=0;
         int b=0;
         int c=0;
-        for(auto &i:s){
-            a+=(i=='a');
-            b+=(i=='b');
-            c+=(i=='c');
+        int n=s.size();
+        while(i<n && ((a<k) || (b<k) ||(c<k))){
+            a+=(s[i]=='a');
+            b+=(s[i]=='b');
+            c+=(s[i]=='c');
+            i++;
         }
-        if(k==0){
-            return 0;
-        }
-        if(a<k || b<k || c<k){
+        if((a<k) || (b<k) ||(c<k)){
             return -1;
         }
         else{
-            int A=0;
-            int B=0;
-            int C=0;
-            int n=s.size();
-            int i=0;
-            while(A<k || B<k || C<k){
-            A+=(s[i]=='a');
-            B+=(s[i]=='b');
-            C+=(s[i]=='c');
-            i++;
-            }
-            int sol = i;
-            i--;
+        int ans=i;
             int j=n-1;
-            // int j=n-1;
-            while(i>=0){
-            A-=(s[i]=='a');
-            B-=(s[i]=='b');
-            C-=(s[i]=='c');
-                while((A<k || B<k || C<k) && j>=i){
-                    A+=(s[j]=='a');
-                    B+=(s[j]=='b');
-                    C+=(s[j]=='c');
-                    j--;
-                }
-                int tt = i+n-1-j;
-                if(A>=k && B>=k && C>=k){
-                sol = min(sol,tt);
-                }
-                i--;
+            i--;
+        while(i>=0){
+            a-=(s[i]=='a');
+            b-=(s[i]=='b');
+            c-=(s[i]=='c');
+            while((j>=0) && ((a<k) || (b<k) ||(c<k))){
+                a+=(s[j]=='a');
+                b+=(s[j]=='b');
+                c+=(s[j]=='c');
+                j--;
             }
-        return sol;
-            
+            if(a>=k && b>=k && c>=k){
+                int mid= (i) + (n-j-1);
+                ans = min(ans,mid);
+            }
+            i--;
+                
         }
-    
+            
+        
+        return ans;
+        }
     }
 };
