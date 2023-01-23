ef17412454a03daf214d43934705bb4ba3b36eb1
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-01-23 09:35:38 +0530

    Time: 1898 ms (16.67%), Space: 188.2 MB (50.00%) - LeetHub

--- 2547-minimum-cost-to-split-an-array/2547-minimum-cost-to-split-an-array.cpp
+++ 2547-minimum-cost-to-split-an-array/2547-minimum-cost-to-split-an-array.cpp
@@ -4,17 +4,19 @@ public:
         int n=nums.size();
         vector<long long int>dp(n,INT_MAX);
         dp[0]=k;
+        unordered_map<int,int>m;
+        
         for(int i=1;i<n;i++){
-            unordered_map<int,int>m;
+            m.clear();
             m[nums[i]]++;
             long long int mid=0;
             for(int j=i-1;j>=0;j--){
                 dp[i]=min(dp[i],k+mid+dp[j]);
-                m[nums[j]]++;
-                if(m[nums[j]]==2){
+                int x = ++m[nums[j]];
+                if(x==2){
                     mid+=2;
                 }
-                else if(m[nums[j]]>1){
+                else if(x>1){
                     mid++;
                 }
             }
