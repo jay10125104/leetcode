4798fdd55af00af72914647cb00c43f2ecc20de7
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-01-28 07:02:10 +0530

    Time: 182 ms (12.55%), Space: 33.3 MB (62.74%) - LeetHub

---
+++ 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.cpp
@@ -0,0 +1,43 @@
+class SummaryRanges {
+public:
+    vector<vector<int>>ans;
+    vector<int>v;
+    int mx=-1;
+    SummaryRanges() {
+        v.resize(10001);
+    }
+    
+    void addNum(int x) {
+        mx = max(mx,x);
+        v[x]++;
+    }
+    
+    vector<vector<int>> getIntervals() {
+        ans.clear();
+        int prev=-2;
+        int prevv=-2;
+        for(int i=0;i<=mx;i++){
+            if((prev+1)==i && v[i]){
+                prev=i;
+            }
+            else if(v[i]){
+                if(prevv!=-2){
+                    ans.push_back({prevv,prev});
+                }
+                prevv=i;
+                prev=i;
+            }
+        }
+        ans.push_back({prevv,prev});
+        return ans;
+        
+    }
+    
+};
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges* obj = new SummaryRanges();
+ * obj->addNum(value);
+ * vector<vector<int>> param_2 = obj->getIntervals();
+ */
