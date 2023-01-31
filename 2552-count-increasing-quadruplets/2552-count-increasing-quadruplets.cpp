83ad142efa34614c2e5259f79b83b9895d4c384e
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-01-31 22:43:50 +0530

    Time: 1169 ms (52.96%), Space: 258.3 MB (38.27%) - LeetHub

--- 2552-count-increasing-quadruplets/2552-count-increasing-quadruplets.cpp
+++ 2552-count-increasing-quadruplets/2552-count-increasing-quadruplets.cpp
@@ -11,11 +11,7 @@ public:
                 if(nums[i]>nums[j]){
                     count++;
                 }
-                // cout<<left[i][j]<<" ";
             }
-            // cout<<endl;
-            
-            
         }
         for(int i=0;i<n;i++){
             int count=0;
@@ -24,23 +20,12 @@ public:
                 if(nums[i]<nums[j]){
                     count++;
                 }
-                // cout<<ri?ght[i][j]<<" ";
-            }
-            // cout<<endl;
-            
-            
+            }           
         }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         cout<<right[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
         long long int ans=0;
         for(int i=1;i<n;i++){
             for(int j=i+1;j<n-1;j++){
                 if(nums[i]>nums[j]){
-                    // cout<<nums[i]<<" "<<nums[j]<<endl;
                 int first = left[j][i];
                 int second = right[i][j];
                 ans+=(first*second);
@@ -48,6 +33,6 @@ public:
             }
         }
         return ans;
-        return -1;
+        // return -1;
     }
 };
