af835f10588db6eb13adbe9304b46ddb11c6bb81
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-02-05 00:14:09 +0530

    Time: 522 ms (9.09%), Space: 37.2 MB (9.09%) - LeetHub

---
+++ 2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>&grid,int i,int j){
+        if(i>=grid.size() || j>=grid[0].size() || grid[i][j]==0){
+            return false;
+        }
+        if((i==(grid.size()-1)) && j==(grid[0].size()-1)){
+            return true;
+            
+        }
+        grid[i][j]=0;
+        if(dfs(grid,i+1,j)){
+            return true;
+        }
+        if(dfs(grid,i,j+1)){
+            return true;
+        }
+        return false;
+    }
+    bool isPossibleToCutPath(vector<vector<int>>& grid) {
+        if(dfs(grid,0,0)==false){
+            return true;
+        }
+        for(auto &i:grid){
+            for(auto &j:i){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+        grid[0][0]=1;
+        if(dfs(grid,0,0)==false){
+            return true;
+        }
+        return false;
+    }
+};
