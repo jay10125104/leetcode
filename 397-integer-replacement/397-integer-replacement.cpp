dc45526f3e9693db2392f671af11a66db399b32b
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-02-06 14:58:13 +0530

    Time: 3 ms (66.07%), Space: 9.9 MB (12.34%) - LeetHub

---
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    map<long long int,long long int>m;
+    long long int ans(long long int n){
+        if(n==1){
+            return 0;
+        }
+        else if(m.find(n)!=m.end()){
+            return m[n];
+        }
+        else if(n%2){
+            return m[n] = (long long int)(1+min(ans(n-1),ans(n+1)));
+        }
+        else{
+            return m[n] = 1+ans(n/2);
+        }
+    }
+    int integerReplacement(int n) {
+        return ans(n);
+    }
+};
