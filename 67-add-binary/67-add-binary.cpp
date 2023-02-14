911cbc1f7756e4c1766af40014fa610418851034
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-02-14 08:41:23 +0530

    Time: 5 ms (42.01%), Space: 6.4 MB (61.65%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,76 +1,36 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-  
-string ans = "";
- int i=a.size()-1;
-    int j=b.size()-1;
-    int carry = 0;
-    
-    while(i>=0 && j>=0){
-        int sum = a[i]-'0' + b[j]-'0' + carry;
-        
-        if(sum==0){
-            ans = ans + '0';
-            carry = 0;
+        if(b.size()>a.size()){
+            swap(a,b);
         }
-        else if(sum==1){
-            ans = ans + '1';
-            carry = 0;
-        } 
-        else if(sum==2){
-            ans =ans +  '0';
-            carry = 1;
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        int i=0;
+        int c=0;
+        // cout<<a<<" ";
+        while(i<b.size()){
+            int mid = c + a[i]-'0' + b[i]-'0';
+            a[i] = (char)(mid%2+'0');
+            c = mid/2;
+            i++;
+        // cout<<a<<" ";
+            
         }
-        else if(sum==3){
-            ans = ans +  '1';
-            carry = 1;
-        }           
-        i--;
-        j--;
-    }
-    
-    while(i>=0){
-        int sum = a[i]-'0' + carry;
-        if(sum==0){
-            ans = ans + '0';
-            carry = 0;
+        if(c){
+            
+            while(i<a.size()){
+            int mid = c + a[i]-'0';
+            a[i] =(char)(mid%2+'0');
+            c = mid/2;
+            i++;
+                // cout<<a<<" ";
+            }
+            if(c){
+                a+="1";
+            }
         }
-        else if(sum==1){
-            ans = ans + '1';
-            carry = 0;
-        } 
-        else if(sum==2){
-            ans = ans + '0';
-            carry = 1;
-        }
-        i--;
-    }
-    while(j>=0){
-        int sum = b[j]-'0' + carry;
-        
-        if(sum==0){
-            ans =ans +  '0';
-            carry = 0;
-        }
-        else if(sum==1){
-            ans = ans + '1';
-            carry = 0;
-        } 
-        else if(sum==2){
-            ans = ans + '0';
-            carry = 1;
-        }
-        j--;
-    }
-    if(carry==1) {
-        ans = ans + to_string(carry);
-    }
-    reverse(ans.begin(), ans.end());
-
-    
-    return ans;
-    
- 
+        reverse(begin(a),end(a));
+        return a;
     }
 };
