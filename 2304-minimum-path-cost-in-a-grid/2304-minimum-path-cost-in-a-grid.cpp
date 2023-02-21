8da15d5dd6a0f57821b6ac3e10dcc38c70051eef
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-02-21 17:45:57 +0530

    Time: 384 ms (37.91%), Space: 80.1 MB (77.47%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int dp[51][51];
     int ans(vector<vector<int>>&v,vector<vector<int>>&c,int row,int col){
-        if(row==v.size()-1){
-            return v[row][col];
-        }
-        if(dp[row][col]!=-1){
+       if(row==v.size()-1){
+           return v[row][col];
+       }
+        else if(dp[row][col]!=-1){
             return dp[row][col];
         }
-        int mn = INT_MAX;
+        int mn=INT_MAX;
         for(int i=0;i<v[0].size();i++){
-            mn=min(mn,v[row][col]+ans(v,c,row+1,i)+c[v[row][col]][i]);
+            mn = min(mn,v[row][col]+ans(v,c,row+1,i)+c[v[row][col]][i]);
         }
         return dp[row][col] = mn;
     }
