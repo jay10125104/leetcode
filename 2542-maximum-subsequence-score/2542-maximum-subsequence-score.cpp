7507f173a63793e48e0c499c77f326bc96eda3f6
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-03-17 23:18:27 +0530

    Time: 249 ms (93.50%), Space: 97.1 MB (33.06%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        priority_queue<int,vector<int>,greater<int>>q;
+        vector<pair<int,int>>v;
+        int n=nums1.size();
+        vector<int>t(n);
+        for(int i=0;i<nums1.size();i++){
+            v.push_back({nums2[i],nums1[i]});
+            
+        }
+        sort(v.begin(),v.end());
+        for(int i=0;i<v.size();i++){
+            t[i] = v[i].second;
+        }
+        long long int sum=0;
+        vector<long long int>ans(n);
+        for(int i=n-1;i>=0;i--){
+            q.push(v[i].second);
+            ans[i]=sum;
+            sum+=v[i].second;
+            if(q.size()>=k){
+                sum-=q.top();
+                q.pop();
+            }
+            // cout<<ans[i]<<" ";
+            
+        }
+        long long int sol=0;
+        for(int i=0;i<v.size()-k+1;i++){
+            sol = max(sol,(long long int)v[i].first*(v[i].second+ans[i]));
+            // cout<<sol<<" ";
+        }
+        // cout<<endl;
+        return sol;
+        
+    }
+};
