72e7e650c9c505a8996c559c11b7a3bf76a747e3
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-03-21 21:02:15 +0530

    Time: 379 ms (14.32%), Space: 87.8 MB (10.11%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,52 +1,60 @@
 class Solution {
 public:
-    long long int global = 0;
-    long long int temp[1000000];
-    void merge(vector<int> &nums,int l,int mid,int r){
-        long long int i = l;
-        long long int j = mid+1;
-        long long int k = l;
+    // int count2 = 0;
+    int t[1000000];
+    long long int merge(vector<int>&v,int i,int mid,int j){
+        int sz = (j-i+1);
         
-        while(i<=mid and j <= r){
-            if(nums[i] <= nums[j]){
-                temp[k++] = nums[i++];
+        int count=0;
+        int idx1=i;
+        int idx2=mid+1;
+        long long int ans=0;
+        while(idx1<=mid && idx2<=j){
+            if(v[idx1]>v[idx2]){
+                ans+=(mid-idx1+1);
+                t[count++]=v[idx2];
+                idx2++;
             }
             else{
-                temp[k++] = nums[j++];
-                global+=(mid-i+1);
+                t[count++]=v[idx1];
+                idx1++;
+            }
+        }
+        while(idx1<=mid){
+            t[count++]=v[idx1++];
+        }
+        while(idx2<=j){
+            t[count++]=v[idx2++];
+        }
+        count=0;
+        for(int k=i;k<=j;k++){
+            v[k]=t[count++];
+        }
+        return ans;
+    }
+    long long int mergesort(vector<int>&nums,int i,int j){
+        if(i==j){
+            return 0;
+        }
+        else{
+            long long int mid=(i+j)/2;
+            long long int left = mergesort(nums,i,mid); 
+            long long int right = mergesort(nums,mid+1,j);
+            long long int midd = merge(nums,i,mid,j);
+            return left+midd+right;
+        }
+    }
+    bool isIdealPermutation(vector<int>& nums) {
+        int count1=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<nums[i-1]){
+                count1++;
             }
         }
         
-        while(i <= mid){
-            temp[k++] = nums[i++];
-        }
-        
-        while(j <= r){
-            temp[k++] = nums[j++];
-        }
-        
-        for(long long int i = l; i <= r;i++){
-            nums[i] = temp[i];
-        }
-    }
-    
-    void Mergesort(vector<int>&nums,int l,int r){
-        if(l>=r) return;
-        int mid = (l+r)/2;
-        
-        Mergesort(nums,l,mid);
-        Mergesort(nums,mid+1,r);
-        merge(nums,l,mid,r);
-    }
-    
-    bool isIdealPermutation(vector<int>& nums) {
-        int local = 0;
-        for(int i = 0; i < nums.size()-1; i++) {
-            if(nums[i] > nums[i+1])
-                local++;
-        }
-        Mergesort(nums,0,nums.size()-1);
-        
-        return global == local ? 1 : 0;
+        // cout<<count2<<endl;
+        int count2 = mergesort(nums,0,nums.size()-1);
+        // cout<<count2<<endl;
+        return count1==count2;
     }
 };
