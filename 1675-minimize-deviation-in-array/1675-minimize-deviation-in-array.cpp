d51e1486300e706bf914f13a8f98b527123b1600
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-03-24 23:21:32 +0530

    Time: 1492 ms (5.08%), Space: 251.5 MB (6.26%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    struct data_struct{
+    multiset<int>m;
+    data_struct(){
+
+    }
+    void insert(int x){
+        m.insert(x);
+    }
+    void remove(int x){
+        m.erase(m.find(x));
+    }
+    void erase(int x){
+        m.erase(x);
+    }
+    bool count(int x){
+        return m.count(x);
+    }
+    int size(){
+        return m.size();
+    }
+    int lower_bound(int x){
+        auto it =  m.lower_bound(x);
+        if(it==m.end()){
+            return -1;
+        }
+        else{
+            return *it;
+        }
+    }
+    auto upper_bound(int x){
+        auto it =  m.upper_bound(x);
+        if(it==m.end()){
+            return -1;
+        }
+        else{
+            return *it;
+        }
+    }
+    int max(){
+        return *(--m.end());
+    }
+    int min(){
+        return *(m.begin());
+    }
+};
+
+    int minimumDeviation(vector<int>& nums) {
+        data_struct d = data_struct();
+        for(auto &i:nums){
+            if(i%2){
+                d.insert(i*2);
+            }
+            else{
+                d.insert(i);
+            }
+        }
+        int ans = INT_MAX;
+        while(d.size()){
+            int x = d.max();
+            // cout<<x<<endl;
+            int mn=d.min();
+                ans = min(ans,x-mn);
+            if(x%2){
+                break;
+            }
+            else{
+                
+                d.remove(x);
+                d.insert(x/2);
+            }
+        }
+        return ans;
+    }
+};
