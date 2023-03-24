4d907243e6b30eea7e61a43dad1e7f5dc7a21193
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-03-24 22:16:39 +0530

    Time: 243 ms (32.51%), Space: 47.2 MB (61.73%) - LeetHub

---
+++ 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int eatenApples(vector<int>& apples, vector<int>& days) {
+        int ans=0;
+        int n=apples.size();
+        int idx=0;
+        priority_queue<pair<int,int>>q;
+        for(int i=0;i<=100000;i++){
+            // if(q.size()){
+            //     cout<<q.top().first<<" "<<q.top().second<<endl;
+            // }
+            // else{
+            //     cout<<"*\n";
+            // }
+            while(q.size() && (abs(q.top().first)<=i||q.top().second==0)){
+                q.pop();
+            }
+            if(i<n){
+                if(apples[i])
+                q.push({(i+days[i])*-1,apples[i]});
+                // idx++;
+            }
+            if(q.size()){
+                auto it = q.top();
+                q.pop();
+                int x = it.first;
+                int y = it.second;
+                y--;
+                if(y)
+                q.push({x,y});
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
