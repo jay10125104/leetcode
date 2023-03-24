7783fe4fd84e066418ca9564d473a7380fc8f94b
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-03-24 19:53:39 +0530

    Time: 515 ms (5.09%), Space: 77.8 MB (19.44%) - LeetHub

---
+++ 1648-sell-diminishing-valued-colored-balls/1648-sell-diminishing-valued-colored-balls.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& inventory, int k) {
+        priority_queue<long long int>q;
+        q.push(0);
+        long long int level=0;
+        for(auto &i:inventory){
+            q.push(i);
+        }
+        int mod = 1000000007;
+        long long int ans=0;
+        while(q.size()){
+            level++;
+            long long int x = q.top();
+            if(x==0){
+                break;
+            }
+            else{
+                q.pop();
+                long long int y = q.top();
+                if(x==y){
+                    continue;
+                }
+                long long int diff = x-y;
+                long long int canmade = (diff)*level;
+                // int div = 
+                cout<<canmade<<endl;
+                if(canmade>=k){
+                    long long int div = k/level;
+                    long long int rem = k%level;
+                    long long int newval = x-div;
+                    // cout<<div<<" "<<rem<<" "<<newval<<" "<<level<<endl;
+                    ans+=((((div*(2*x-div+1)))/2)*level);
+                    ans+=(rem*newval);
+                    k=0;
+                    
+                }
+                else{
+                    k-=canmade;
+                    // cout<<canmade<<" "<<(((diff)*(x+y+1))/2)*level<<endl;            
+                    ans+=(((diff)*(x+y+1))/2)*level;
+                }
+            }
+        }
+        return ((ans%mod + mod)%mod)%mod;
+        
+    }
+};
