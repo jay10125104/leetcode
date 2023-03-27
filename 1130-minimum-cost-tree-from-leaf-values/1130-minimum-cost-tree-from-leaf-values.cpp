ff690f18579d4991b824b7e15fc02145123e9663
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-03-28 01:48:48 +0530

    Time: 9 ms (73.21%), Space: 8.3 MB (95.06%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -4,27 +4,24 @@ public:
         int n=v.size();
         int dp[n][n];
         memset(dp,0,sizeof(dp));
+        int mx[n][n];
+        memset(mx,0,sizeof(mx));
+        for(int i=n-1;i>=0;i--){
+            int mn=0;
+            for(int j=i;j<n;j++){
+                mn = max(mn,v[j]);
+                mx[i][j]=mn;
+            }
+        }
         for(int i=n-1;i>=0;i--){
             for(int j=i;j<n;j++){
                 dp[i][j] = INT_MAX;
-                if(i==j){
-                    dp[i][j] = 0;
-                }
-                else if((i+1)==j){
-                    dp[i][j]=v[i]*v[j];
+                if(j==i){
+                    dp[i][j]=0;
                 }
                 else{
                     for(int k=i;k<j;k++){
-                        int mx1 = 0;
-                        int mx2 = 0;
-                        for(int t = i;t<=k;t++){
-                            mx1 = max(mx1,v[t]);
-                        }
-                        for(int t=k+1;t<=j;t++){
-                            mx2 = max(mx2,v[t]);
-                        }
-                        // cout<<mx1<<" "<<mx2<<" "<<i<<" "<<j<<" "<<endl;
-                        dp[i][j] = min(dp[i][j],(mx1*mx2)+dp[i][k] + dp[k+1][j]);
+                        dp[i][j] = min(dp[i][j],mx[i][k]*mx[k+1][j]+dp[i][k]+dp[k+1][j]);
                     }
                 }
                 // cout<<dp[i][j]<<" ";
