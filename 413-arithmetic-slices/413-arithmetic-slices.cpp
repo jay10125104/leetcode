9fceea1ccd43a0ec4e1afcc2abcb726a5e432b15
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-03-29 00:27:33 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (58.89%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& v) {
+    int numberOfArithmeticSlices(vector<int>& v){
+       int ans=0;
+        int count=0;
         int n=v.size();
-        set<pair<int,int>>dp;
-        int ans=0;
-        // cout<<"df";
-        // memset(dp,0,sizeof(dp));
-        for(int i=n-1;i>=0;i--){
-            for(int j=i;j<n;j++){
-                int len = (j-i+1);
-                if(len==3){
-                    bool x = ((v[i+1]-v[i])==(v[i+2]-v[i+1]));
-                    if(x)
-                        dp.insert({i,j});
-                }
-                else if(len>3){
-                    if(dp.count({i,j-1})){
-                    bool x = ((v[j]-v[j-1])==(v[j-1]-v[j-2]));
-                        // dp[i][j]=x;
-                        if(x)
-                        dp.insert({i,j});
-                    }
-                }
-                ans+=dp.count({i,j});
+        for(int i=2;i<n;i++){
+            if((v[i]-v[i-1]) ==( v[i-1]-v[i-2])){
+                count++;
             }
+            else{
+                count=0;
+            }
+            cout<<count<<endl;
+            ans+=count;
         }
-        return ans;
+    return ans;
     }
+    
+    
 };
