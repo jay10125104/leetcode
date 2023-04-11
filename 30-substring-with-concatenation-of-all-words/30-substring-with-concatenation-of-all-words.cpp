00d75efa8eca16d68310875fbae73a43aa2d373a
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-04-11 13:10:13 +0530

    Time: 1655 ms (7.75%), Space: 452 MB (10.12%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    map<string,int>m;
+    bool check(string s,int sz){
+        map<string,int>m1;
+        for(int i=0;i+sz<=s.size();i+=sz){
+            string mid = s.substr(i,sz);
+            m1[mid]++;
+        }
+        return m1==m;
+    }
+    vector<int> findSubstring(string s, vector<string>& t) {
+        vector<int>ans;
+        int n=t.size();
+        int req = t[0].size()*n;
+        for(auto &i:t){
+            m[i]++;
+        }
+        int sz = t[0].size();
+        for(int i=0;i+req<=s.size();i++){
+            string mid = s.substr(i,req);
+            // cout<<mid<<endl;
+            if(check(mid,(int)t[0].size())){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
