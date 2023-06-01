7eebb0d0f410d68f719517e5e976edb7d36cb693
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-06-01 23:49:45 +0530

    Time: 36 ms (85.66%), Space: 10.7 MB (72.43%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -5,14 +5,10 @@ public:
         int n=t.size();
         int dp[m+1][n+1];
         memset(dp,0,sizeof(dp));
-        dp[0][0]=1;
         int ans=0;
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0||j==0){
-                    dp[i][j]=1;
-                }
-                else if(s[i-1]==t[j-1]){
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+               if(s[i-1]==t[j-1]){
                     dp[i][j]=dp[i-1][j-1]+1;
                 }
                 else{
@@ -22,6 +18,6 @@ public:
             }
             
         }
-        return ans-1;
+        return ans;
     }
 };
