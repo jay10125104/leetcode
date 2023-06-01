a97d6a8e7c6e0b310eb5762c4d18fa987dc339ac
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-06-01 10:30:01 +0530

    Time: 1753 ms (19.54%), Space: 282.8 MB (31.96%) - LeetHub

---
+++ 2713-maximum-strictly-increasing-cells-in-a-matrix/2713-maximum-strictly-increasing-cells-in-a-matrix.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int maxIncreasingCells(vector<vector<int>>& v) {
+        unordered_map<int,vector<pair<int,int>>>idx;
+        unordered_map<int,int>value;
+        int m=v.size();
+        int n=v[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                value[v[i][j]]++;
+                idx[v[i][j]].push_back({i,j});
+            }
+        }
+        int dp[m][n];
+        vector<int>t;
+        for(auto &i:value){
+            t.push_back(i.first);
+        }
+        sort(t.rbegin(),t.rend());
+        memset(dp,0,sizeof(dp));
+        
+        map<int,int>row;
+        map<int,int>col;
+        int ans=0;
+        for(auto &i:t){
+                vector<int>mid;
+            for(auto &k:idx[i]){
+                    int x = k.first;
+                    int y = k.second;
+                    int mx=max(row[x],col[y])+1;
+                // cout<<i<<" "<<x<<" "<<y<<" "<<mx<<endl;
+                    mid.push_back(mx);
+                    ans=max(ans,mx);
+            }
+                int count=0;
+                for(auto &k:idx[i]){
+                    int x = k.first;
+                    int y = k.second;
+                    row[x]=max(row[x],mid[count]);
+                    col[y]=max(col[y],mid[count++]);
+                }
+            
+        }
+        return ans;
+        
+        
+        
+        
+    }
+};
