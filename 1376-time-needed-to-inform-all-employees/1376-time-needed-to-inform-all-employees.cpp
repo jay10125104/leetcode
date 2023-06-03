dd4d5118bbef02c5714d32490b12af9c9d442ac2
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-06-03 07:00:10 +0530

    Time: 428 ms (30.57%), Space: 131.1 MB (27.36%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dfs(vector<int>adj[],int src,int parent,vector<int>&time){
+        int count=0;
+        int ans=0;
+        for(auto &i:adj[src]){
+            if(i!=parent){
+                count++;
+                ans=max(ans,dfs(adj,i,src,time));
+            }
+        }
+        if(count){
+            return time[src]+ans;
+        }
+        else{
+            return 0;
+        }
+    }
+    int numOfMinutes(int n, int headID, vector<int>& v, vector<int>& time) {
+        int src=-1;
+        vector<int>adj[n];
+        for(int i=0;i<n;i++){
+            if(v[i]!=-1){
+                adj[v[i]].push_back(i);
+                adj[i].push_back(v[i]);
+            }
+        }
+        return dfs(adj,headID,-1,time);
+    }
+};
