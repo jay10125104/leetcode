1d7f69b42e76057671b06c7bb18928d64cfe603c
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-06-03 15:57:19 +0530

    Time: 7 ms (87.67%), Space: 14 MB (71.65%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int visited[51][51];
-    void dfs(vector<vector<int>>&v,int x,int y,int color){
+    void dfs(vector<vector<int>>&v,int x,int y,int color,int ncolor){
         if(x<0||y<0||x==v.size()||y==v[0].size()||visited[x][y]==1||v[x][y]!=color){
             return;
         }
         visited[x][y]=1;
-        dfs(v,x+1,y,color);
-        dfs(v,x-1,y,color);
-        dfs(v,x,y+1,color);
-        dfs(v,x,y-1,color);
+        v[x][y]=ncolor;
+        dfs(v,x+1,y,color,ncolor);
+        dfs(v,x-1,y,color,ncolor);
+        dfs(v,x,y+1,color,ncolor);
+        dfs(v,x,y-1,color,ncolor);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& v, int sr, int sc, int color) {
         memset(visited,0,sizeof(visited));
-        dfs(v,sr,sc,v[sr][sc]);
-        for(int i=0;i<v.size();i++){
-            for(int j=0;j<v[0].size();j++){
-                if(visited[i][j]){
-                    v[i][j]=color;
-                }
-            }
-        }
+        dfs(v,sr,sc,v[sr][sc],color);
         return v;
     }
 };
