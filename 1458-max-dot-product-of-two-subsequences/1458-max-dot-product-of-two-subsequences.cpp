5c352c6b3b4b91d47437a9cb8489d3fee940b5d8
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-10-08 07:59:21 +0530

    Time: 13 ms (93.32%), Space: 10.9 MB (75.84%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    long long int dp[501][501][2];
-    long long int ans(vector<int>&nums1,vector<int>&nums2,int idx1,int idx2,int cnt1){
-        if((idx1==nums1.size() || nums2.size()==idx2) && (cnt1==0)){
-            return INT_MIN;
-        }
-        else if(idx1==nums1.size() || nums2.size()==idx2){
-            return 0;
-        }
-        else if(dp[idx1][idx2][cnt1] !=-1 ){
-            return dp[idx1][idx2][cnt1];
-        }
-        else{
-            return dp[idx1][idx2][cnt1] = max({nums1[idx1]*nums2[idx2] + ans(nums1,nums2,idx1+1,idx2+1,1),ans(nums1,nums2,idx1+1,idx2,cnt1),ans(nums1,nums2,idx1,idx2+1,cnt1)});
-        }
+    int dp[501][501];
+    long long solve(vector<int>&nums1, vector<int>&nums2,int i,int j,int n,int m)
+    {
+        if(i>=n || j>=m)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        
+        return dp[i][j]=max(solve(nums1,nums2,i+1,j,n,m),max(solve(nums1,nums2,i,j+1,n,m),(nums1[i]*nums2[j])+solve(nums1,nums2,i+1,j+1,n,m)));
     }
-    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+    long long maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         memset(dp,-1,sizeof(dp));
-        return ans(nums1,nums2,0,0,0);
+        int ans=solve(nums1,nums2,0,0,nums1.size(),nums2.size()),n=nums1.size(),m=nums2.size();
+		//id your answer is 0 that means your max dot product is negative
+        if(ans!=0)return ans;
+		//then do maximum of(min from num1 and max from nums2 ) , f(max from num1 and min from nums2 )
+        int mini=*min_element(nums1.begin(),nums1.end()),maxi=*max_element(nums2.begin(),nums2.end());
+        ans=mini*maxi;
+		mini=*min_element(nums2.begin(),nums2.end()),maxi=*max_element(nums1.begin(),nums1.end());
+        ans=max(ans,mini*maxi);
+        return ans;
     }
 };
