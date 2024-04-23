78648f20b7c983173fc429414ba2ec0bf4d24097
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2024-04-23 06:37:58 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (22.22%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-       
-        double ans = (double)(pow(x,n));
+        int m=n;
+        double ans=1;
+        double mid=1;
+        while(n){
+            if(n&1){
+                ans=ans*x;
+            }
+            x=x*x;
+            n/=2;
+        }
+        if(m<0){
+            ans=1.0/ans;
+        }
         return ans;
-        
-    
-        
     }
 };
